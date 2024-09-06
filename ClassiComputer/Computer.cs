e693746dc1a5cdfec0c25ab45c82a534763cfe12
Federico Giuliano <fede.giuliano2001@gmail.com>
2024-09-07 00:07:27 +0200

    es computer

---
+++ ClassiComputer/Computer.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassiComputer
+{
+    public class Computer
+    {
+       // dimensioneHd, vlocità processore,
+        public int DimensioneHd { get; set; }
+        public int VelocitaProcessore { get; set; }
+
+        private enum Status
+        {
+            acceso, standBy, spento
+        }
+        private Status _status = Status.spento;
+
+        public Computer(int dimensione, int velocita)
+        {
+            this.DimensioneHd = dimensione;
+            this.VelocitaProcessore = velocita;
+        }
+
+        public delegate void AccesoDelegate();
+        public event AccesoDelegate AccesoIniziato;
+
+        public delegate void StandByDelegate();
+        public event StandByDelegate StandByIniziato;
+
+        public delegate void SpentoDelegate();
+        public event SpentoDelegate SpentoIniziato;
+
+        public void Acceso() 
+        {
+            Console.WriteLine("Acceso Fired");
+            if (this._status != Status.acceso) 
+            {
+                this._status = Status.acceso;
+                if (AccesoIniziato != null) AccesoIniziato();
+            }
+        }
+
+        public void StandBy()
+        {
+            Console.WriteLine("StandBy Fired");
+            if (this._status == Status.acceso)
+            {
+                this._status = Status.standBy;
+                if (StandByIniziato != null) StandByIniziato();
+            } else if (this._status == Status.spento) 
+            {
+                throw new Exception("Operazione non Permessa");
+            }
+        }
+
+        public void Spento()
+        {
+            Console.WriteLine("Spento Fired");
+            if (this._status == Status.acceso)
+            {
+                this._status = Status.spento;
+                if (SpentoIniziato != null) SpentoIniziato();
+            }
+            else if (this._status == Status.standBy)
+            {
+                throw new Exception("Operazione non Permessa");
+            }
+        }
+
+    }
+}
---
+++ Programmacomputer/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassiComputer;
+
+namespace Programmacomputer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Computer c = new Computer(1080, 100);
+
+            c.AccesoIniziato += OnAccesoIniziato;
+            c.StandByIniziato += OnStandByIniziato;
+            c.SpentoIniziato += OnSpentoIniziato;
+
+            c.Acceso();
+            c.StandBy();
+            c.Acceso();
+            c.Spento();
+
+            Console.ReadLine();
+        }
+
+        static void OnAccesoIniziato() { Console.WriteLine("OnAccesoIniziato Fired"); }
+        static void OnStandByIniziato() { Console.WriteLine("OnStandByIniziato Fired"); }
+        static void OnSpentoIniziato() { Console.WriteLine("OnSpentoIniziato Fired"); }
+
+    }
+}
